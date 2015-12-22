f853ac8fef4cd6498f3b30caf040e6c6aff080d2
Jack Evans <jack@evans.gb.net>
2015-12-22 23:47:43 +0000

    Added comments, improved code clarity

--- problem3part1.lisp
+++ problem3part1.lisp
@@ -15,16 +15,16 @@
     ((eql direction #\<) '(-1 0))))
 
 
-;; Travel down each route recursively
+;; Creates a list of stops along santas journey
 (defun get-stops (vecs &key (stops (list '(0 0))))
   (if vecs
     (let ((newX (+ (car (first vecs)) (car (first stops))))
           (newY (+ (cadr (first vecs)) (cadr (first stops)))))
       (get-stops (rest vecs) :stops (cons (list newX newY) stops)))
-    (length(remove-duplicates stops :test #'equal))))
+    (remove-duplicates stops :test #'equal)))
 
 
 ;; Map through each direction in the list
 (let ((directions-list (read-in-file "day3input.txt")))
   (let ((vectors (remove nil(map 'list #'get-direction directions-list))))
-    (format t "~d~%" (get-stops vectors))))
+    (format t "~d~%" (length (get-stops vectors)))))
