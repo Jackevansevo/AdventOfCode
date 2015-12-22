251b2050c425dc1a25e1c82851f63596ae55392b
Jack Evans <jack@evans.gb.net>
2015-12-22 03:12:55 +0000

    Rewrote in functional style

--- problem3part1.lisp
+++ problem3part1.lisp
@@ -14,24 +14,27 @@
     ((eql direction #\v) '(0 -1))
     ((eql direction #\<) '(-1 0))))
 
+;; [TODO] Rewrite this using recursion instead of maps baby
 
-;; Travels down each route collecting a list of pos
+; ;; Travels down each route collecting a list of pos
+; (defun travel-routes (vectors)
+;   (let ((pos '(0 0)))
+;     (let ((stops (map 'list (travel pos) vectors)))
+;       (length (remove-duplicates(append (list pos) stops) :test #'equal)))))
+
+
+; ;; Travels down a route
+; (defun travel (pos)
+;   (lambda (vec)
+;     (let ((newX (+ (car vec) (car pos)))
+;           (newY (+ (cadr vec) (cadr pos))))
+;       (setq pos (list newX newY))
+;       pos)))
+
 (defun travel-routes (vectors)
-  (let ((pos '(0 0)))
-    (let ((stops (map 'list (travel pos) vectors)))
-      (length (remove-duplicates(append (list pos) stops) :test #'equal)))))
-
-
-;; Travels down a route
-(defun travel (pos)
-  (lambda (vec)
-    (let ((newX (+ (car vec) (car pos)))
-          (newY (+ (cadr vec) (cadr pos))))
-      (setq pos (list newX newY))
-      pos)))
-
+  (format t "List of vectors: ~d~%" vectors))
 
 ;; Map through each direction in the list
 (let ((directions-list (read-in-file "day3input.txt")))
   (let ((vectors (remove nil(map 'list #'get-direction directions-list))))
-    (format t "Unique houses: ~d~%" (travel-routes vectors))))
+    (travel-routes vectors)))
