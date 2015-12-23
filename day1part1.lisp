b539fbb67da73e15a4af94c0148fe85b9ad39bf9
Jack Evans <jack@evans.gb.net>
2015-12-23 01:11:08 +0000

    Renamed problem files

---
+++ day1part1.lisp
@@ -0,0 +1,8 @@
+(defun count-parens (input)
+  (cond
+    ((eql input #\() 1)
+    ((eql input #\)) -1)))
+
+(let ((in (open "day1input.txt")))
+  (princ (eval(cons '+ (map 'list #'count-parens (read-line in)))))
+  (close in))
---
+++ day3part1.lisp
@@ -0,0 +1,30 @@
+;; Open the file and loop through each character
+(defun read-in-file (filename)
+  (with-open-file (in filename)
+    (loop for line = (read-char in nil)
+          while line
+          collect line)))
+
+
+;; Converts direction symbols into vectors
+(defun get-direction (direction)
+  (cond
+    ((eql direction #\^) '(0 1))
+    ((eql direction #\>) '(1 0))
+    ((eql direction #\v) '(0 -1))
+    ((eql direction #\<) '(-1 0))))
+
+
+;; Creates a list of stops along santas journey
+(defun get-stops (vecs &key (stops (list '(0 0))))
+  (if vecs
+    (let ((newX (+ (car (first vecs)) (car (first stops))))
+          (newY (+ (cadr (first vecs)) (cadr (first stops)))))
+      (get-stops (rest vecs) :stops (cons (list newX newY) stops)))
+    (remove-duplicates stops :test #'equal)))
+
+
+;; Map through each direction in the list
+(let ((directions-list (read-in-file "day3input.txt")))
+  (let ((vectors (remove nil(map 'list #'get-direction directions-list))))
+    (format t "~d~%" (length (get-stops vectors)))))
